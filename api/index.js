68539c3f0d7906dd7a6f31bfd1bb70b743ec99f1
Abhishek Bhardwaj <ayushindia4@gmail.com>
2025-05-28 20:36:55 +0530

    fixed some backend errors

--- api/index.js
+++ api/index.js
@@ -10,16 +10,18 @@ const cookieParser = require('cookie-parser');
 const multer = require('multer');
 const uploadMiddleware = multer({ dest: 'uploads/' });
 const fs = require('fs');
+const dotenv = require('dotenv')
+
+dotenv.config({})
 
 const salt = bcrypt.genSaltSync(10);
-const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-mongoose.connect('mongodb+srv://abhiktd15:9g3EYIYONV9ZFIVf@cluster0.sic7gp4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(console.log("database connected"))
+mongoose.connect(process.env.MONGODB_URI).then(console.log("database connected"))
 
 app.post('/register', async (req,res) => {
     const {username,password} = req.body;
@@ -41,7 +43,7 @@ app.post('/login', async (req,res) => {
     const passOk = bcrypt.compareSync(password, userDoc.password);
     if (passOk) {
         // logged in
-        jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+        jwt.sign({username,id:userDoc._id}, process.env.JWT_SECRET, {}, (err,token) => {
         if (err) throw err;
         res.cookie('token', token).json({
             id:userDoc._id,
@@ -55,7 +57,7 @@ app.post('/login', async (req,res) => {
 
 app.get('/profile', (req,res) => {
     const {token} = req.cookies;
-    jwt.verify(token, secret, {}, (err,info) => {
+    jwt.verify(token, process.env.JWT_SECRET, {}, (err,info) => {
         if (err) throw err;
         res.json(info);
     });
@@ -73,7 +75,7 @@ app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
     fs.renameSync(path, newPath);
 
     const {token} = req.cookies;
-    jwt.verify(token, secret, {}, async (err,info) => {
+    jwt.verify(token, process.env.JWT_SECRET, {}, async (err,info) => {
         if (err) throw err;
         const {title,summary,content} = req.body;
         const postDoc = await Post.create({
@@ -99,7 +101,7 @@ app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
     }
 
     const {token} = req.cookies;
-    jwt.verify(token, secret, {}, async (err,info) => {
+    jwt.verify(token, process.env.JWT_SECRET, {}, async (err,info) => {
         if (err) throw err;
         const {id,title,summary,content} = req.body;
         const postDoc = await Post.findById(id);
